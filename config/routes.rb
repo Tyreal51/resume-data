d9c0662ad48ae72fe599536c1f19e94bbee25df3
Joe Trask <YOUR@EMAIL.com>
2018-05-01 18:24:20 -0500

    routes3

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  # get '/students' => 'students#index'
-  # get '/students/:the_id' => 'students#show'
-  # post '/students' => 'students#create'
-  # patch '/students/:id' => 'students#update'
-  # delete '/students/:id' => 'students#destroy'
-  # resources :educations
-  # resources :skills
-  # resources :capstones
-  # resources :experiences
+  get '/students' => 'students#index'
+  get '/students/:the_id' => 'students#show'
+  post '/students' => 'students#create'
+  patch '/students/:id' => 'students#update'
+  delete '/students/:id' => 'students#destroy'
+  resources :educations
+  resources :skills
+  resources :capstones
+  resources :experiences
   
-    resources :students do
-        resources :educations, :skills, :capstones, :experiences
-    end
+    # resources :students do
+    #     resources :educations, :skills, :capstones, :experiences
+    # end
   
 end
